6346897609709575d28cfba8f579050886b569d9
Andory <andorys@gmail.com>
2018-03-03 22:33:25 +0200

    added the slave class

---
+++ SadnaSrc/SadnaSrc/Slave.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SadnaSrc
+{
+    class Slave
+    {
+        public void speak()
+        {
+            Console.WriteLine("I'm a slave...");
+        }
+    }
+}
