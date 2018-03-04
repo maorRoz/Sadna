bdeb919dc50856976e0c426eec866fa3387a45ef
pninabsh <pninabsh@gmail.com>
2018-03-04 20:49:54 +0200

    this is the best change in the world

--- SadnaSrc/SadnaSrc/Slave.cs
+++ SadnaSrc/SadnaSrc/Slave.cs
@@ -15,6 +15,8 @@ namespace SadnaSrc
             Console.WriteLine("added another update");
             Console.WriteLine("added another junk");
             Console.WriteLine("I love choclate");
+			int i = 42;
+			Console.WriteLine("This is the best number: " + i);
         }
     }
 }
