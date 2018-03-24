3a12aa5ef34635763a1198ced88a11bc7acb9021
maorRoz <andory@post.bgu.ac.il>
2018-03-24 20:21:58 +0300

    added early structure of whitebox + blackbox testing

---
+++ SadnaSrc/UserSpotTests/UserTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UserSpotTests
+{
+    [TestClass]
+    public class UserTest
+    {
+        private int stam = 0;
+        [TestInitialize]
+        public void stamNo()
+        {
+            stam = 2;
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1,stam);
+        }
+
+        [TestMethod]
+        public void fuckyou()
+        {
+            Assert.AreEqual(2,stam);
+        }
+    }
+}
