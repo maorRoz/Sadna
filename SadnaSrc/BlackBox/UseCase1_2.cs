44b93640a61625d99bd13441613c748806b500bd
pninabsh <pninabsh@gmail.com>
2018-03-27 22:50:31 +0300

    Created the sturcutre of the test for use case 1.2 - user registration

---
+++ SadnaSrc/BlackBox/UseCase1_2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackBox
+{
+	[TestClass]
+	public class UseCase1_2
+	{
+		private UserBridge _bridge;
+
+		[TestInitialize]
+		public void MarketBuilder()
+		{
+			_bridge = new RealBridge();
+		}
+		[TestMethod]
+		public void TestMethod1()
+		{
+
+		}
+
+		[TestCleanup]
+		public void UserTestCleanUp()
+		{
+
+		}
+	}
+}
