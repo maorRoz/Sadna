f3e59265a2d6eb9b281c941f01fc79ca5af2f194
Igor8757 <igor8757@gmail.com>
2018-03-28 14:36:55 +0300

    Basic Implementation of Order and OrderItem, Added OrderService class.

--- SadnaSrc/SadnaSrc/OrderPool/Order.cs
+++ SadnaSrc/SadnaSrc/OrderPool/Order.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,71 @@ namespace SadnaSrc.OrderPool
 {
     class Order
     {
-       
+        private List<OrderItem> _items;
+        private double _totalPrice;
+        private string _shippingAddress;
+        private string _userName;
+        private int _orderId;
+
+        public List<OrderItem> GetItems() { return _items; }
+        public double GetPrice() { return _totalPrice; }
+        public string GetShippingAddress() { return _shippingAddress; }
+        public string GetUserName() { return _userName; }
+        public int GetOrderID() { return _orderId; }
+
+        public Order(string userName)
+        {
+            _userName = userName;
+            _shippingAddress = "";
+            _items = new List<OrderItem>();
+            _orderId = new Random().Next(100000, 999999);
+        }
+
+        public Order(string userName, string shippingAddress)
+        {
+            _userName = userName;
+            _shippingAddress = shippingAddress;
+            _items = new List<OrderItem>();
+            _orderId = new Random().Next(100000, 999999);
+        }
+
+        public bool CheckOrderItem(string name,string store)
+        {
+            foreach (OrderItem item in _items)
+            {
+                if (item.GetName() == name && item.GetStore() == store)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public void AddOrderItem(OrderItem item)
+        {
+            if (CheckOrderItem(item.GetName(), item.GetStore()))
+            {
+                throw new OrderException("Order item is already listed in the order.");
+            }
+            else
+            {
+                _items.Add(item);
+                _totalPrice += item.GetPrice();
+            }
+        }
+
+        public void RemoveOrderItem(OrderItem item)
+        {
+            if (!CheckOrderItem(item.GetName(), item.GetStore()))
+            {
+                throw new OrderException("Order item is not listed in the order.");
+            }
+            else
+            {
+                _items.Remove(item);
+                _totalPrice -= item.GetPrice();
+
+            }
+        }
+
     }
 }
--- SadnaSrc/SadnaSrc/OrderPool/OrderItem.cs
+++ SadnaSrc/SadnaSrc/OrderPool/OrderItem.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SadnaSrc.UserSpot;
 
 namespace SadnaSrc.OrderPool
 {
     class OrderItem
     {
+        private string _store;
+        private string _name;
+        private double _price;
+        private int _quantity;
+
+        public OrderItem(string store, string name, double price, int quantity)
+        {
+            _store = store;
+            _name = name;
+            _price = price;
+            _quantity = quantity;
+        }
+
+        // TODO add price when there will be getters.
+        public OrderItem(CartItem item)
+        {
+            _store = item.GetStore();
+            _name = item.GetName();
+            _quantity = item.GetQuantity();
+
+
+        }
+
+        public string GetStore() { return _store; }
+        public string GetName() { return _name; }
+        public double GetPrice() { return _price; }
+        public int GetQuantity() { return _quantity; }
+
+
+
     }
 }
