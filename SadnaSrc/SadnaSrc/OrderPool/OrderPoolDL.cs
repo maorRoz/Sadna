0e450eed44d877e90d203b055a5fe0ad3ec58776
Igor8757 <igor8757@gmail.com>
2018-03-28 14:36:55 +0300

    Basic Implementation of Order and OrderItem, Added OrderService class.

--- SadnaSrc/SadnaSrc/OrderPool/OrderPoolDL.cs
+++ SadnaSrc/SadnaSrc/OrderPool/OrderPoolDL.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SadnaSrc.Main;
 
 namespace SadnaSrc.OrderPool
 {
-    class OrderPoolDL
+    class OrderPoolDL : systemDL
     {
+        public OrderPoolDL(SQLiteConnection dbConnection) : base(dbConnection)
+        {
+        }
     }
 }
