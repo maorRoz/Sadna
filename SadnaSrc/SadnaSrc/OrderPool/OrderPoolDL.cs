9ad3725e973e763ee8183315e136a30e72665442
Igor8757 <igor8757@gmail.com>
2018-03-28 18:02:01 +0300

    Fixed order fields and added simple DB support

--- SadnaSrc/SadnaSrc/OrderPool/OrderPoolDL.cs
+++ SadnaSrc/SadnaSrc/OrderPool/OrderPoolDL.cs
@@ -13,5 +13,39 @@ namespace SadnaSrc.OrderPool
         public OrderPoolDL(SQLiteConnection dbConnection) : base(dbConnection)
         {
         }
+
+        public Order FindOrder(int id)
+        {
+            Order order = null;
+            var dbReader = SelectFromTableWithCondition("Order", "*", "ID = " + id + "");
+            while (dbReader.Read())
+            {
+                if (dbReader.GetValue(0) != null)
+                {
+                    order = new Order(dbReader.GetInt32(0), dbReader.GetString(1), dbReader.GetString(2));
+                }
+            }
+            return order;
+        }
+
+        public List<OrderItem> FindOrderItemsOfOrder(int id)
+        {
+            List<OrderItem> list = new List<OrderItem>();
+            var dbReader = SelectFromTableWithCondition("OrderItemsToOrders JOIN OrderItem", "*", "OrderID = " + id + "");
+            while (dbReader.Read())
+            {
+                if (dbReader.GetValue(0) != null)
+                {
+                    //list.Add(new OrderItem( args ));
+                    // TODO see how the joined table is buikd and add the order items accordingly
+                }
+            }
+            return list;
+        }
+
+        public void AddOrder(Order order)
+        {
+
+        }
     }
 }
