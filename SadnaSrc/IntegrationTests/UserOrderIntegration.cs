9e31e2d72766cd63d89a23459cb974e676c318c6
ZoharK93 <zoharkapustin@gmail.com>
2018-04-02 17:07:49 +0300

    Created a new test class and intiaized fields

---
+++ SadnaSrc/IntegrationTests/UserOrderIntegration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SadnaSrc.Main;
+using SadnaSrc.OrderPool;
+using SadnaSrc.StoreCenter;
+using SadnaSrc.SupplyPoint;
+using SadnaSrc.UserSpot;
+using SadnaSrc.Walleter;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class UserOrderIntegration
+    {
+        private UserService userServiceSession;
+        private OrderService orderServiceSession;
+        private StoreService storeServiceSession;
+        private MarketYard marketSession;
+        private string userName = "Buyer1";
+        private string userPass = "123";
+
+        [TestInitialize]
+        public void MarketBuilder()
+        {
+            marketSession = MarketYard.Instance;
+            userServiceSession = (UserService)marketSession.GetUserService();
+            storeServiceSession = (StoreService) marketSession.GetStoreService(userServiceSession);
+            orderServiceSession =
+                (OrderService) marketSession.GetOrderService(userServiceSession, storeServiceSession, new SupplyService(),
+                    new PaymentService());
+        }
+
+        [TestMethod]
+        public void TestConvertCartToOrder()
+        {
+
+        }
+    }
+}
