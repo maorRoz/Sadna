6454c31a83f3b53713d799e7f4687bbf11d69cd5
pninabsh <pninabsh@gmail.com>
2018-04-09 19:05:43 +0300

    Added test cases for changing quantity of cart items and removing items from cart.

--- SadnaSrc/BlackBox/UseCase1_6.cs
+++ SadnaSrc/BlackBox/UseCase1_6.cs
@@ -51,6 +51,126 @@ namespace BlackBoxUserTests
 
 		}
 
+		[TestMethod]
+		public void DidntEnterSystemViewCart()
+		{
+			Assert.AreEqual((int) ViewCartStatus.DidntEnterSystem, _bridge.ViewCart().Status);
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void SuccessChangeQuantityOfProduct()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			//TODO: change a quantity of an item
+			//TODO: call viewCart and compare between expected and received products
+			//TODO: change quantity back to what it was before
+			//TODO: print to see it worked
+			//TODO: the last two should probably be a function
+
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void DidntEnterSystemChangeQuantity()
+		{
+			Assert.AreEqual((int)EditCartItemStatus.DidntEnterSystem, _bridge.EditCartItem("M", "Gun", 25.0, 5).Status);
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void NoItemFoundChangeQuantityStoreName()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			Assert.AreEqual((int)EditCartItemStatus.NoItemFound, _bridge.EditCartItem("Moo", "Gun", 25.0, 5).Status);
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void NoItemFoundChangeQuantityProductName()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			Assert.AreEqual((int)EditCartItemStatus.NoItemFound, _bridge.EditCartItem("M", "Gunoooo", 25.0, 5).Status);
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void NoItemFoundChangeQuantityUnitPrice()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			Assert.AreEqual((int)EditCartItemStatus.NoItemFound, _bridge.EditCartItem("M", "Gun", 50, 5).Status);
+		}
+
+
+
+		
+		[TestMethod]
+		public void NegativeQuantityInserted()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			Assert.AreEqual((int)EditCartItemStatus.ZeroNegativeQuantity, _bridge.EditCartItem("M", "Gun", 25.0, -200).Status);
+		}
+		
+
+		[TestMethod]
+		public void ZeroQuantityInserted()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			Assert.AreEqual((int)EditCartItemStatus.ZeroNegativeQuantity, _bridge.EditCartItem("M", "Gun", 25.0, -3).Status);
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void RemoveProductFromCartSuccess()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			//TODO: remove an item
+			//TODO: check the removal was successful
+			//TODO: add the item to the cart
+
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void DidntEnterSystemRemoveItem()
+		{
+			_bridge.RemoveFromCart("M", "Gun", 25.0);
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void NoItemFoundRemoveItemStore()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			Assert.AreEqual((int)RemoveFromCartStatus.NoItemFound,_bridge.RemoveFromCart("Moo","Gun",25.0).Status);
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void NoItemFoundRemoveItemProduct()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			Assert.AreEqual((int)RemoveFromCartStatus.NoItemFound, _bridge.RemoveFromCart("M", "Gunoo", 25.0).Status);
+		}
+
+		//TODO: add another test for a guest
+		[TestMethod]
+		public void NoItemFoundRemoveItemPrice()
+		{
+			_bridge.EnterSystem();
+			_bridge.SignIn(userToCheck, userToCheckPassword);
+			Assert.AreEqual((int)RemoveFromCartStatus.NoItemFound, _bridge.RemoveFromCart("M", "Gun", 20).Status);
+		}
+
 		[TestCleanup]
 		public void UserTestCleanUp()
 		{
