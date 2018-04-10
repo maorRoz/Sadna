dbdefb8324532fc887d9f545a5e79bd9890a7caa
ZoharK93 <zoharkapustin@gmail.com>
2018-04-10 16:50:43 +0300

    Fixed bugs according to review

--- SadnaSrc/IntegrationTests/UserSpot_OrderPool_IntegrationTest.cs
+++ SadnaSrc/IntegrationTests/UserSpot_OrderPool_IntegrationTest.cs
@@ -17,6 +17,8 @@ namespace IntegrationTests
         private UserBuyerHarmony userBuyerHarmony;
         private MarketYard marketSession;
 
+        private IUserService userServiceSession2;
+
         private string user = "Vadim Chernov";
         private string pass = "123";
 
@@ -25,7 +27,9 @@ namespace IntegrationTests
         {
             marketSession = MarketYard.Instance;
             userServiceSession = (UserService) marketSession.GetUserService();
+            userServiceSession2 = (UserService)marketSession.GetUserService();
             userServiceSession.EnterSystem();
+            userServiceSession2.EnterSystem();
             orderServiceSession = (OrderService) marketSession.GetOrderService(ref userServiceSession);
             userBuyerHarmony = new UserBuyerHarmony(ref userServiceSession);
         }
@@ -71,8 +75,8 @@ namespace IntegrationTests
             {
                 userServiceSession.SignIn(user, pass);
                 orderServiceSession.BuyItemFromImmediate("Bamba", "The Red Rock", 1, 6.00);
-                userServiceSession.SignIn(user, pass);
-                CartItem item = ((UserService) userServiceSession).MarketUser.Cart.SearchInCart("The Red Rock", "Bamba", 6.00);
+                userServiceSession2.SignIn(user, pass);
+                CartItem item = ((UserService) userServiceSession2).MarketUser.Cart.SearchInCart("The Red Rock", "Bamba", 6.00);
                 //Assert.AreEqual(2,item.Quantity);
             }
             catch (MarketException)
@@ -88,8 +92,8 @@ namespace IntegrationTests
             {
                 userServiceSession.SignIn(user, pass);
                 orderServiceSession.BuyItemFromImmediate("OCB", "24", 2, 10.00);
-                userServiceSession.SignIn(user, pass);
-                //Assert.IsNull(((UserService)userServiceSession).MarketUser.Cart.SearchInCart("24", "OCB", 10.00));
+                userServiceSession2.SignIn(user, pass);
+                //Assert.IsNull(((UserService)userServiceSession2).MarketUser.Cart.SearchInCart("24", "OCB", 10.00));
             }
             catch (MarketException)
             {
@@ -104,8 +108,8 @@ namespace IntegrationTests
             {
                 userServiceSession.SignIn(user, pass);
                 orderServiceSession.BuyItemFromImmediate("Bamba", "The Red Rock", 999, 6.00);
-                userServiceSession.SignIn(user, pass);
-                CartItem item = ((UserService)userServiceSession).MarketUser.Cart.SearchInCart("The Red Rock", "Bamba", 6.00);
+                userServiceSession2.SignIn(user, pass);
+                CartItem item = ((UserService)userServiceSession2).MarketUser.Cart.SearchInCart("The Red Rock", "Bamba", 6.00);
                 //Assert.AreEqual(3,item.Quantity);
             }
             catch (MarketException)
@@ -121,8 +125,8 @@ namespace IntegrationTests
             {
                 userServiceSession.SignIn(user, pass);
                 orderServiceSession.BuyItemFromImmediate("Bamba", "The Red Rock", -5, 6.00);
-                userServiceSession.SignIn(user, pass);
-                CartItem item = ((UserService)userServiceSession).MarketUser.Cart.SearchInCart("The Red Rock", "Bamba", 6.00);
+                userServiceSession2.SignIn(user, pass);
+                CartItem item = ((UserService)userServiceSession2).MarketUser.Cart.SearchInCart("The Red Rock", "Bamba", 6.00);
                 //Assert.AreEqual(3,item.Quantity);
             }
             catch (MarketException)
@@ -138,8 +142,8 @@ namespace IntegrationTests
             {
                 userServiceSession.SignIn(user, pass);
                 orderServiceSession.BuyAllItemsFromStore("The Red Rock");
-                userServiceSession.SignIn(user, pass);
-                //Assert.AreEqual(0, ((UserService)userServiceSession).MarketUser.Cart.GetCartStorage("The Red Rock"));
+                userServiceSession2.SignIn(user, pass);
+                //Assert.AreEqual(0, ((UserService)userServiceSession2).MarketUser.Cart.GetCartStorage("The Red Rock"));
             }
             catch (MarketException)
             {
@@ -154,8 +158,8 @@ namespace IntegrationTests
             {
                 userServiceSession.SignIn(user, pass);
                 orderServiceSession.BuyAllItemsFromStore("The Blue Rock");
-                userServiceSession.SignIn(user, pass);
-                //Assert.AreEqual(3, ((UserService)userServiceSession).MarketUser.Cart.GetCartStorage());
+                userServiceSession2.SignIn(user, pass);
+                //Assert.AreEqual(3, ((UserService)userServiceSession2).MarketUser.Cart.GetCartStorage());
             }
             catch (MarketException)
             {
@@ -170,8 +174,8 @@ namespace IntegrationTests
             {
                 userServiceSession.SignIn(user, pass);
                 orderServiceSession.BuyEverythingFromCart();
-                userServiceSession.SignIn(user, pass);
-                //Assert.AreEqual(0, ((UserService)userServiceSession).MarketUser.Cart.GetCartStorage());
+                userServiceSession2.SignIn(user, pass);
+                //Assert.AreEqual(0, ((UserService)userServiceSession2).MarketUser.Cart.GetCartStorage());
             }
             catch (MarketException)
             {
@@ -212,6 +216,7 @@ namespace IntegrationTests
         public void UserOrderTestCleanUp()
         {
             userServiceSession.CleanSession();
+            userServiceSession2.CleanSession();
             orderServiceSession.CleanSession();
             userBuyerHarmony.CleanSession();
             MarketYard.CleanSession();
