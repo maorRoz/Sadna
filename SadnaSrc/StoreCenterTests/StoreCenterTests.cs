2e719fba2fe4317aef6c456d645b42adedf2ccc3
lior <lior.ashiri@gmail.com>
2018-04-11 20:54:27 +0300

    many changes, commit to creathe clean session

---
+++ SadnaSrc/StoreCenterTests/StoreCenterTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SadnaSrc.Main;
+using SadnaSrc.StoreCenter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreCenterTests
+{
+    [TestClass]
+    class OpenStoreTests
+    {
+        private MarketYard market;
+        private ModuleGlobalHandler handler;
+        [TestInitialize]
+        public void BuildStore()
+        {
+            market = MarketYard.Instance;
+            handler = ModuleGlobalHandler.GetInstance();
+        }
+        [TestMethod]
+        public void CheckName()
+        {
+            Store A = new Store("SX", "name1test", "here");
+            Store find = handler.DataLayer.GetStoreByName("SX");
+            Assert.IsNull(find);
+            Assert.IsTrue(handler.CheckStoreNameUnique("SX"));
+            handler.DataLayer.AddStore(A);
+            find = handler.DataLayer.GetStoreByName("SX");
+            Assert.IsFalse(handler.CheckStoreNameUnique("SX"));
+            handler.DataLayer.RemoveStore(A);
+        }
+    }
+}
