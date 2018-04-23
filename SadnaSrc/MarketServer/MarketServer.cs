638f724ce9d3408acd808d2d32f0b05a2bb49f03
maorRoz <andory@post.bgu.ac.il>
2018-04-24 01:15:16 +0300

    added basic functionality to the MarketServer

--- SadnaSrc/MarketServer/MarketServer.cs
+++ SadnaSrc/MarketServer/MarketServer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SadnaSrc.Main;
 using WebSocketManager;
 
 
@@ -9,14 +10,25 @@ namespace MarketServer
 {
     public class MarketServer : WebSocketHandler
     {
+        private List<IUserService> users;
+        private MarketYard marketSession;
         public MarketServer(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
         {
-
+            marketSession = MarketYard.Instance;
+            users = new List<IUserService>();
         }
 
         public async Task SendMessage(string socketId, string message)
         {
             await InvokeClientMethodToAllAsync("marketMessage", socketId, message);
+        }
+
+        public async Task EnterSystem()
+        {
+            IUserService user = marketSession.GetUserService();
+            user.EnterSystem();
+            users.Add(user);
+            await InvokeClientMethodToAllAsync("identifyClient", 3003);
         }
     }
 }
--- SadnaSrc/MarketServer/wwwroot/js/site.js
+++ SadnaSrc/MarketServer/wwwroot/js/site.js
@@ -5,28 +5,17 @@ $(document).ready(function() {
 
     connection.connectionMethods.onConnected = () => {
         console.log('client has been connected!');
+        connection.invoke('EnterSystem', connection.connectionId, message);
     }
 
     connection.connectionMethods.onDisconnected = () => {
         console.log('client has been disconnected!');
     }
-    connection.clientMethods['marketMessage'] = (socketId, message) => {
-        var messageText = socketId + 'said : ' + message;
-        $('#messages').append('<li>' + messageText +'</li>')
-        console.log(messageText);
+
+    connection.clientMethods['identifyClient'] = (userId) => {
+        var messageText = 'your user id is:' + userId;
+        console.log(userId);
     }
     connection.start();
 
-    var $messagecontent = $('#message-content');
-    $messagecontent.keyup(function(e) {
-        if (e.keyCode === 13) {
-            var message = $messagecontent.val().trim();
-            if (message.length === 0) {
-                return false;
-            }
-
-            connection.invoke('SendMessage', connection.connectionId, message);
-            $messagecontent.val('');
-        }
-    });
 })
