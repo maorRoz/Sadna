ba366920a36afec79bbc507bb380c7a1003202ad
maorRoz <andory@post.bgu.ac.il>
2018-04-28 06:18:06 +0300

    added increase and decrease quantity methods to view cart

--- SadnaSrc/MarketServer/Controllers/CartController.cs
+++ SadnaSrc/MarketServer/Controllers/CartController.cs
@@ -12,6 +12,7 @@ namespace MarketWeb.Controllers
 {
     public class CartController : Controller
     {
+        private int Success = 0;
         public IActionResult CartManagement(int systemId, string state,string message)
         {
             var userService = MarketServer.users[systemId];
@@ -19,10 +20,25 @@ namespace MarketWeb.Controllers
             return View(new CartModel(systemId, state, message, cartData));
         }
 
+        public IActionResult IncreaseCartItem(int systemId, string state, string store, string product, double unitPrice)
+        {
+            var userService = MarketServer.users[systemId];
+            userService.EditCartItem(store, product, 1, unitPrice);
+            return RedirectToAction("CartManagement", new { systemId, state });
+        }
+
+        public IActionResult DecreaseCartItem(int systemId, string state, string store, string product, double unitPrice)
+        {
+            var userService = MarketServer.users[systemId];
+            var answer = userService.EditCartItem(store, product,-1, unitPrice);
+            var message = answer.Status == 0 ? null : answer.Answer;
+            return RedirectToAction("CartManagement", new { systemId, state,message });
+        }
+
         public IActionResult RemoveCartItem(int systemId, string state, string store, string product, double unitPrice)
         {
             var userService = MarketServer.users[systemId];
-            var answer = userService.RemoveFromCart(store, product, unitPrice);
+            userService.RemoveFromCart(store, product, unitPrice);
             return RedirectToAction("CartManagement", new { systemId, state });
         }
     }
