77cc7115483450b496c5d122a41e23005288171a
maorRoz <andory@post.bgu.ac.il>
2018-05-01 13:19:18 +0300

    Established Moq in the MarketFeedTests project

--- SadnaSrc/MarketFeedTests/UnitTests/PublisherTests.cs
+++ SadnaSrc/MarketFeedTests/UnitTests/PublisherTests.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SadnaSrc.MarketFeed;
+using Moq;
 
 namespace MarketFeedTests.UnitTests
 {
     [TestClass]
     public class PublisherTests
     {
+
+        private Publisher publisher;
+
+        [TestInitialize]
+        public void PublisherTestBuilder()
+        {
+            publisher = Publisher.Instance;
+            
+        }
+
+
         [TestMethod]
-        public void TestMethod1()
+        public void QueueForEachUserTest()
         {
         }
     }
