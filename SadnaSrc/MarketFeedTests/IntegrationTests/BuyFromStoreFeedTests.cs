a5ba9d0080edf8dcfd320ec76fdc0308b7f8bf63
maorRoz <andory@post.bgu.ac.il>
2018-05-04 21:51:56 +0300

    added mock in marketfeed to not produce errors and message logs

--- SadnaSrc/MarketFeedTests/IntegrationTests/BuyFromStoreFeedTests.cs
+++ SadnaSrc/MarketFeedTests/IntegrationTests/BuyFromStoreFeedTests.cs
@@ -21,6 +21,9 @@ namespace MarketFeedTests.IntegrationTests
         [TestInitialize]
         public void IntegrationFeedTestsBuilder()
         {
+            var marketDbMocker = new Mock<IMarketDB>();
+            MarketException.SetDB(marketDbMocker.Object);
+            MarketLog.SetDB(marketDbMocker.Object);
             countMessagesToServer = 0;
             serverMocker = new Mock<IListener>();
             serverMocker.Setup(x => x.GetMessage(owner1.ToString(), "#9 has been sold in Cluckin Bell!")).Callback(SendMessageToServer);
--- SadnaSrc/MarketFeedTests/IntegrationTests/LotteryFinishFeedTests.cs
+++ SadnaSrc/MarketFeedTests/IntegrationTests/LotteryFinishFeedTests.cs
@@ -24,6 +24,9 @@ namespace MarketFeedTests.IntegrationTests
         [TestInitialize]
         public void IntegrationFeedTestsBuilder()
         {
+            var marketDbMocker = new Mock<IMarketDB>();
+            MarketException.SetDB(marketDbMocker.Object);
+            MarketLog.SetDB(marketDbMocker.Object);
             countMessagesToServer = 0;
             serverMocker = new Mock<IListener>();
             serverMocker.Setup(x => x.GetMessage(buyerId2.ToString(), "You've won the lottery on " + productLottery + " in " 
