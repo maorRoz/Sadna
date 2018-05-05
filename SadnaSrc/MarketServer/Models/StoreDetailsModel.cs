84bbf11f9df15694f8c9c5b32d035aa21be41576
pninabsh <pninabsh@gmail.com>
2018-05-05 10:38:33 +0300

    Added a model for storeInfo

---
+++ SadnaSrc/MarketServer/Models/StoreDetailsModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarketServer.Models;
+
+namespace MarketWeb.Models
+{
+    public class StoreDetailsModel:UserModel
+    {
+	    private StoreItemModel[] Items { get; set; }
+		public StoreDetailsModel(int systemId, string state, string message, string[] itemData) : base(systemId, state, message)
+	    {
+		    Items = new StoreItemModel[itemData.Length];
+		    for (var i = 0; i < Items.Length; i++)
+		    {
+			    Items[i] = new StoreItemModel(itemData[i]);
+		    }
+		}
+
+	    private class StoreItemModel
+		{
+			private string Name { get; set; }
+			private string Address { get; set; }
+
+		    public StoreItemModel(string data)
+		    {
+			    var dataParam = data.Split(new[] { "Name : ", " Address : "}, StringSplitOptions.RemoveEmptyEntries);
+			    Name = dataParam[0];
+			    Address = dataParam[1];
+		    }
+	    }
+	}
+}
