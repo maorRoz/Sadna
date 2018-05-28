e3e828bfd6ee59e091ce56a5c7450d5d5051a174
ZoharK93 <zoharkapustin@gmail.com>
2018-05-28 14:44:39 +0300

    Added tests for Get all categories

--- SadnaSrc/StoreCenterTests/StoreCenterDbIntegrationTests/GetAllCategoriesTests.cs
+++ SadnaSrc/StoreCenterTests/StoreCenterDbIntegrationTests/GetAllCategoriesTests.cs
@@ -14,16 +14,45 @@ namespace StoreCenterTests.StoreCenterDbIntegrationTests
 	class GetAllCategoriesTests
 	{
 		private MarketYard market;
-		public Store toDeleteStore;
-		private IStoreDL handler;
+	    private IStoreShoppingService storeService;
 		IUserService userService;
+
+
 		[TestInitialize]
 		public void BuildInitialize()
 		{
 			MarketDB.Instance.InsertByForce();
 			market = MarketYard.Instance;
-			handler = StoreDL.Instance;
 			userService = market.GetUserService();
+		    storeService = market.GetStoreShoppingService(ref userService);
 		}
-	}
+
+	    [TestMethod]
+	    public void GetCategoriesTest()
+	    {
+	        string[] expected = { "WanderlandItems", "Books" };
+	        userService.EnterSystem();
+	        MarketAnswer ans = storeService.GetAllCategoryNames();
+            Assert.AreEqual((int)GetCategoriesStatus.Success, ans.Status);
+            string[] actual = ans.ReportList;
+            Assert.AreEqual(expected.Length, actual.Length);
+	        for (int i = 0; i < expected.Length; i++)
+	        {
+	            Assert.AreEqual(expected[i], actual[i]);
+	        }
+	    }
+
+	    [TestMethod]
+	    public void DidntEnterTest()
+	    {
+            Assert.AreEqual((int)GetCategoriesStatus.DidntEnterSystem, storeService.GetAllCategoryNames().Status);
+	    }
+
+	    [TestCleanup]
+	    public void CleanUpOpenStoreTest()
+	    {
+	        MarketDB.Instance.CleanByForce();
+	        MarketYard.CleanSession();
+	    }
+    }
 }
