65aa3c8fa5fc086201f6f4374dfc533ef9b9e4d5
MaorRoz <andory@post.bgu.ac.il>
2018-06-16 18:21:59 +0300

    fixed some space problems in messages. changed the name of the "purchase policy" in the navigation bar to "admin purchase policy"

--- SadnaSrc/SadnaSrc/StoreCenter/RemoveProductFromCategorySlave.cs
+++ SadnaSrc/SadnaSrc/StoreCenter/RemoveProductFromCategorySlave.cs
@@ -41,7 +41,7 @@ namespace SadnaSrc.StoreCenter
                 MarketLog.Log("StoreCenter", "Product is in category");
                 DataLayerInstance.RemoveProductFromCategory(category.SystemId, product.SystemId);
                 Answer = new StoreAnswer(StoreEnum.Success,
-                    "product" + productName + " removed successfully from category" + categoryName);
+                    "product " + productName + " removed successfully from category " + categoryName);
             }
             catch (StoreException e)
             {
